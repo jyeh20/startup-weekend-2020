aca32bfa37c12fda9b86b07505a8964f7d387ea8
Jonathan Yeh <jmyeh51@gmail.com>
2020-10-17 12:49:21 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 import {BrowserRouter as Router, Switch,Route} from 'react-router-dom' ;
-import Register from './Components/Register';
+import AccountPage from './Components/Register';
 import Dashboard from './HomePage/Dashboard';
 import Navbar from './HomePage/Navbar';
 import Footer from './HomePage/Footer';
-=======
-import AccountPage from './Components/AccountPage';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-  } from "react-router-dom";
->>>>>>> bb4303ac511be51d6496b3f25b7108c9a7e4fdc0
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
     <Router>
       <Navbar />
       <Switch>{/*Switch is to make sure every time we exit the route, it comes back to the / page */}
@@ -26,9 +16,7 @@ function App() {
       </Switch>
     </Router>
     <Footer />
-=======
         <AccountPage />
->>>>>>> bb4303ac511be51d6496b3f25b7108c9a7e4fdc0
     </div>
   );
 }
